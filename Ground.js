17df42eb5cc87f5a480c80ed77be062c744df60c
Donald <connorkaprielian7@gmail.com>
2021-05-27 17:36:19 -0700

    changed color from brown to yellow

--- Ground.js
+++ Ground.js
@@ -11,7 +11,7 @@ class Ground {
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("brown");
+      fill("Yellow");
       rect(pos.x, pos.y, this.width, this.height);
     }
   };
